f1bac917ae72b38d46b2da0366eae1aa05c03e06
dariopmaciel <dariodepaulamaciel@hotmail.com>
2023-11-14 18:26:27 -0300

    Register Bloc

--- lib/features/contacts/register/bloc/contact_register_bloc.dart
+++ lib/features/contacts/register/bloc/contact_register_bloc.dart
@@ -1,9 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'contact_register_state.dart';
 part 'contact_register_event.dart';
-part 'contact_register_bloc.freezed.dart';
 
-// class ContactRegisterBloc extends Bloc{
+
+class ContactRegisterBloc extends Bloc<ContactRegisterEvent, ContactRegisterState>{
+  ContactRegisterBloc():super(const ContactRegisterState.initial());
    
-// }
+}
